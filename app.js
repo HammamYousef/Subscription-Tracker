c400464d936f9b11e2c393bd010aa793c3e89101
Hammam Yousef <h.abutaima@student.aaup.edu>
2025-07-10 23:49:52 +0300

    error handler

--- app.js
+++ app.js
@@ -5,13 +5,20 @@ import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 import connectDB from './database/MongoDB.js';
+import errorMiddleware from './middlewares/error.middleware.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
 app.use('/api/users', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/subscriptions', subscriptionRouter);
+
+app.use(errorMiddleware);
 
 app.get('/', (req, res) => {
     res.send('Welcome to my server!');
