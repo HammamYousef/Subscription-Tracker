9a1fa6b752b1db106e5930a5986cd23d751c89ad
Hammam Yousef <h.abutaima@student.aaup.edu>
2025-07-11 23:07:00 +0300

    Setup upstach

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import connectDB from './database/MongoDB.js';
 import errorMiddleware from './middlewares/error.middleware.js';
 import cookieParser from 'cookie-parser';
 import arcjetMiddleware from './middlewares/arcjet.middleware.js';
+import workflowRouter from './routes/workflow.routes.js';
 
 const app = express();
 
@@ -19,6 +20,7 @@ app.use(arcjetMiddleware);
 app.use('/api/users', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/subscriptions', subscriptionRouter);
+app.use('/api/workflows', workflowRouter);
 
 app.use(errorMiddleware);
 
